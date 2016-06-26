2b0425d527e4aff4dc543c63102f241ff14c9ed5
denarced <denarced@gmail.com>
2016-06-26 20:24:00 +0300

    Download deviations.

--- dafavorites.go
+++ dafavorites.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"crypto/rand"
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
+	"path/filepath"
 	"strings"
+	"time"
 
 	"github.com/denarced/dafavorites/deviantart"
 )
@@ -33,19 +38,86 @@ func fetchItems(username string) []deviantart.RssItem {
 	return rssItems
 }
 
-func downloadImages(rssItems []deviantart.RssItem) {
-	jsonBytes, err := json.Marshal(rssItems)
-	if err == nil {
-		outFilen := "deviations.json"
-		err = ioutil.WriteFile(outFilen, jsonBytes, 0644)
-		if err != nil {
-			log.Printf("Failed to write favorites to %s. Error: %v\n", outFilen, err)
-			fmt.Printf("%s\n", jsonBytes)
-		} else {
-			log.Printf("Favorites successfully written to %s\n", outFilen)
-		}
+// A single saved deviation
+type SavedDeviation struct {
+	RssItem  deviantart.RssItem
+	Filename string
+}
+
+// One full fetch, all deviations, their saved filenames etc.
+type DeviantFetch struct {
+	SavedDeviations []SavedDeviation
+	Timestamp       time.Time
+}
+
+func newUuid() (string, error) {
+	uuid := make([]byte, 16)
+	n, err := io.ReadFull(rand.Reader, uuid)
+	if n != len(uuid) || err != nil {
+		return "", err
+	}
+	// variant bits; see section 4.1.1
+	uuid[8] = uuid[8]&^0xc0 | 0x80
+	// version 4 (pseudo-random); see section 4.1.3
+	uuid[6] = uuid[6]&^0xf0 | 0x40
+	return fmt.Sprintf("%x-%x-%x-%x-%x", uuid[0:4], uuid[4:6], uuid[6:8], uuid[8:10], uuid[10:]), nil
+}
+
+func downloadImages(dirname, url string, dryRun bool) string {
+	uuid, _ := newUuid()
+	pieces := strings.Split(url, "/")
+	fpath := filepath.Join(dirname, uuid, pieces[len(pieces)-1])
+	if dryRun {
+		return fpath
 	} else {
-		fmt.Printf("Error: %s\n", err)
+		dirpath := filepath.Join(dirname, uuid)
+		err := os.MkdirAll(dirpath, 0700)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Failed to create directories for", dirpath)
+			fmt.Fprintln(os.Stderr, err)
+			return ""
+		}
+	}
+
+	src, err := http.Get(url)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Failed to fetch image.")
+		fmt.Fprintln(os.Stderr, err)
+		return ""
+	}
+	defer src.Body.Close()
+
+	dest, err := os.Create(fpath)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Failed to create image file:", fpath)
+		fmt.Fprintln(os.Stderr, err)
+		return ""
+	}
+	defer dest.Close()
+
+	byteCount, err := io.Copy(dest, src.Body)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Failed to copy image to file from", fpath)
+		fmt.Fprintln(os.Stderr, "Count of bytes copied:", byteCount)
+		fmt.Fprintln(os.Stderr, err)
+		return ""
+	}
+
+	return fpath
+}
+
+func toDeviantFetch(rssItems []deviantart.RssItem, dirname string) DeviantFetch {
+	var savedDeviations []SavedDeviation
+	for _, eachItem := range rssItems {
+		aSavedDeviation := SavedDeviation{
+			RssItem:  eachItem,
+			Filename: downloadImages(dirname, eachItem.Url, false),
+		}
+		savedDeviations = append(savedDeviations, aSavedDeviation)
+	}
+	return DeviantFetch{
+		SavedDeviations: savedDeviations,
+		Timestamp:       time.Now(),
 	}
 }
 
@@ -59,7 +131,20 @@ func main() {
 		if len(username) == 0 {
 			fmt.Println("Username can't be empty")
 		} else {
-			downloadImages(fetchItems(username))
+			rssItems := fetchItems(username)
+			dirname, _ := ioutil.TempDir("", "")
+			fmt.Println("Create directory:", dirname)
+			deviantFetch := toDeviantFetch(rssItems, dirname)
+			jsonBytes, err := json.Marshal(deviantFetch)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, "Conversion to json failed.")
+				fmt.Fprintln(os.Stderr, err)
+			} else {
+				err = ioutil.WriteFile("deviantFetch.json", jsonBytes, 0644)
+				if err != nil {
+					fmt.Fprintln(os.Stderr, err)
+				}
+			}
 		}
 	}
 }
--- deviantart/fetch.go
+++ deviantart/fetch.go
@@ -8,13 +8,19 @@ import (
 	"strings"
 )
 
-type RssItem struct {
-	Title  string
-	Link   string
-	Author string
-	Url    string
+type Dimensions struct {
 	Width  int
 	Height int
+}
+
+type RssItem struct {
+	Title           string
+	Link            string
+	Guid            string
+	PublicationDate string
+	Author          string
+	Url             string
+	Dimensions      Dimensions
 }
 
 type RssFile struct {
@@ -22,7 +28,10 @@ type RssFile struct {
 	RssItems []RssItem
 }
 
+// URL to another Deviant Art RSS xml
 type LinkElement struct {
+	// Relation, e.g. "next". Each RSS xml contains x amount of favorite items
+	// and then the URL in "next" contains the next RSS xml that contains more.
 	Rel  string `xml:"rel,attr"`
 	Href string `xml:"href,attr"`
 }
@@ -32,29 +41,65 @@ type ItemCreditElement struct {
 	Value string `xml:",chardata"`
 }
 
+// Example:
+//     <media:content
+//         url="http://pre03.deviantart.net/bbec/th/pre/f/2015/347/b/f/model_no__th_x11_38_by_wojtekfus-d9k1rbm.jpg"
+//         height="670"
+//         width="1192"
+//         medium="image"/>
 type ItemContentElement struct {
 	Url    string `xml:"url,attr"`
 	Width  int    `xml:"width,attr"`
 	Height int    `xml:"height,attr"`
 }
 
+// Contains information on any given favorite deviation.
+//
+// Example:
+//     <item>
+//  	   <title>MODEL NO. TH-X11-38</title>
+//  	   <link>http://wojtekfus.deviantart.com/art/MODEL-NO-TH-X11-38-577869970</link>
+//  	   <guid isPermaLink="true">http://wojtekfus.deviantart.com/art/MODEL-NO-TH-X11-38-577869970</guid>
+//  	   <pubDate>Sun, 13 Dec 2015 16:14:02 PST</pubDate>
+//  	   <media:title type="plain">MODEL NO. TH-X11-38</media:title>
+//  	   <media:keywords></media:keywords>
+//  	   <media:rating>nonadult</media:rating>
+//  	   <media:category label="Sci-Fi">digitalart/paintings/scifi</media:category>
+//  	   <media:credit role="author" scheme="urn:ebu">WojtekFus</media:credit>
+//  	   <media:credit role="author" scheme="urn:ebu">http://a.deviantart.net/avatars/w/o/wojtekfus.jpg?5</media:credit>
+//  	   <media:copyright url="http://wojtekfus.deviantart.com">Copyright 2015 WojtekFus</media:copyright>
+//  	   <media:description type="html">Image done for the workshop in Taipei that&#039;s going to happen next week:&amp;nbsp;&lt;a class=&quot;external&quot; href=&quot;http://www.deviantart.com/users/outgoing?http://www.likmeetup.com/&quot;&gt;www.likmeetup.com/&lt;/a&gt;&lt;br /&gt;&lt;br /&gt;Struggled with it a lot myself, but I&#039;m letting it go, haha &lt;img src=&quot;http://e.deviantart.net/emoticons/s/smile.gif&quot; width=&quot;15&quot; height=&quot;15&quot; alt=&quot;:)&quot; data-embed-type=&quot;emoticon&quot; data-embed-id=&quot;391&quot; title=&quot;:) (Smile)&quot;/&gt; Hope you like it! Love you guys!</media:description>            <media:thumbnail url="http://t02.deviantart.net/FeGyfpR_tb8vGcOarQm_dyvBd7U=/fit-in/150x150/filters:no_upscale():origin()/pre03/bbec/th/pre/f/2015/347/b/f/model_no__th_x11_38_by_wojtekfus-d9k1rbm.jpg" height="84" width="150"/>            <media:thumbnail url="http://t14.deviantart.net/BPI5k4O3FXvK1PF7VJXREIujS0I=/fit-in/300x900/filters:no_upscale():origin()/pre03/bbec/th/pre/f/2015/347/b/f/model_no__th_x11_38_by_wojtekfus-d9k1rbm.jpg" height="169" width="300"/>            <media:thumbnail url="http://t15.deviantart.net/ZS17sMYJv1Whk_q1lyP4DrvdH30=/300x200/filters:fixed_height(100,100):origin()/pre03/bbec/th/pre/f/2015/347/b/f/model_no__th_x11_38_by_wojtekfus-d9k1rbm.jpg" height="169" width="300"/>
+//  	   <media:content url="http://pre03.deviantart.net/bbec/th/pre/f/2015/347/b/f/model_no__th_x11_38_by_wojtekfus-d9k1rbm.jpg" height="670" width="1192" medium="image"/>
+//  	   <description>Image done for the workshop in Taipei that&#039;s going to happen next week:&amp;nbsp;&lt;a class=&quot;external&quot; href=&quot;http://www.deviantart.com/users/outgoing?http://www.likmeetup.com/&quot;&gt;www.likmeetup.com/&lt;/a&gt;&lt;br /&gt;&lt;br /&gt;Struggled with it a lot myself, but I&#039;m letting it go, haha &lt;img src=&quot;http://e.deviantart.net/emoticons/s/smile.gif&quot; width=&quot;15&quot; height=&quot;15&quot; alt=&quot;:)&quot; data-embed-type=&quot;emoticon&quot; data-embed-id=&quot;391&quot; title=&quot;:) (Smile)&quot;/&gt; Hope you like it! Love you guys!&lt;br /&gt;&lt;div&gt;&lt;img src=&quot;http://t15.deviantart.net/ZS17sMYJv1Whk_q1lyP4DrvdH30=/300x200/filters:fixed_height(100,100):origin()/pre03/bbec/th/pre/f/2015/347/b/f/model_no__th_x11_38_by_wojtekfus-d9k1rbm.jpg&quot; alt=&quot;thumbnail&quot; /&gt;&lt;/div&gt;</description>
+//     </item>
 type RssItemElement struct {
-	Title   string              `xml:"title"`
-	Link    string              `xml:"link"`
-	Url     string              `xml:"url"`
-	Width   int                 `xml:"width"`
-	Height  int                 `xml:"height"`
-	Credits []ItemCreditElement `xml:"credit"`
-	Content ItemContentElement  `xml:"content"`
+	Title           string              `xml:"title"`
+	Link            string              `xml:"link"`
+	Guid            string              `xml:"guid"`
+	PublicationDate string              `xml:"pubDate"`
+	Url             string              `xml:"url"`
+	Width           int                 `xml:"width"`
+	Height          int                 `xml:"height"`
+	Credits         []ItemCreditElement `xml:"credit"`
+	Content         ItemContentElement  `xml:"content"`
 }
 
+// The single channel element in Deviant Art RSS xml that's located inside the
+// root rss element.
 type ChannelElement struct {
-	Links    []LinkElement    `xml:"link"`
+	// The link elements. In this bunch we're mostly interested in the "next"
+	// links.
+	Links []LinkElement `xml:"link"`
+	// The actual item elements, each of which contains a single favorite
+	// deviation.
 	RssItems []RssItemElement `xml:"item"`
 }
 
+// The root element of Deviant Art's RSS xml
 type RssElement struct {
-	XMLName xml.Name       `xml:"rss"`
+	XMLName xml.Name `xml:"rss"`
+	// The single channel element in the xml. At least no more than one hasn't
+	// been seen during development.
 	Channel ChannelElement `xml:"channel"`
 }
 
@@ -69,12 +114,16 @@ func itemElementsToItems(elements []RssItemElement) []RssItem {
 			}
 		}
 		rssItems = append(rssItems, RssItem{
-			Title:  each.Title,
-			Link:   each.Link,
-			Author: author,
-			Url:    each.Content.Url,
-			Width:  each.Content.Width,
-			Height: each.Content.Height,
+			Title:           each.Title,
+			Link:            each.Link,
+			Guid:            each.Guid,
+			PublicationDate: each.PublicationDate,
+			Author:          author,
+			Url:             each.Content.Url,
+			Dimensions: Dimensions{
+				Width:  each.Content.Width,
+				Height: each.Content.Height,
+			},
 		})
 	}
 	return rssItems
